4e4481a67283db7fc3fbc86bc1f2bd5adb9f22ac
maxer137 <maxer137@gmail.com>
2017-07-19 21:45:20 +0200

    Update population.js

--- population.js
+++ population.js
@@ -1,23 +1,25 @@
 function Population() {
 
-  this.crossover = function(partnerA, partnerB){
+  this.crossover = function(partnerA, partnerB, Ca, Cb) {
     var newgenes = [];
+    var color;
     var mid = floor(random(partnerA.length));
     for (var i = 0; i < partnerA.length; i++) {
-      if(i > mid) {
+      if (i > mid) {
         newgenes[i] = partnerA[i];
+        color = lerpColor(Ca, Cb, .33);
       } else {
         newgenes[i] = partnerB[i];
       }
     }
-    return new AI(newgenes);
+    return new AI(color, newgenes);
   }
 
-  this.selection = function(){
-    for (var i = 0; i < 100; i++) {
+  this.selection = function() {
+    for (var i = 0; i < popsize.value(); i++) {
       var partnerA = random(nextgen);
       var partnerB = random(nextgen);
-      var child = this.crossover(partnerA.wages, partnerB.wages);
+      var child = this.crossover(partnerA.wages, partnerB.wages, partnerA.color, partnerB.color);
       child.mutation();
       newgen[i] = child;
     }

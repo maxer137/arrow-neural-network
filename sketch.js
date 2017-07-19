6982cad10ae03400452495c301880424645afdf6
maxer137 <maxer137@gmail.com>
2017-07-19 21:45:52 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ var bestnumb;
 var bow;
 var best = 999;
 var population = 100;
+var popsize;
+var tcount = 0; //counter for when to change targets position
 var count = 0;
 var kill = 0;
 var ps = 20;
@@ -14,16 +16,19 @@ var fit = []; //array for fitness values
 
 function setup() {
   createCanvas(600, 450);
+  createP('population size');
+  popsize = createSlider(10, 200, 50);
+
   bow = new bow(50, 300);
   target = new Target();
   population = new Population();
-  for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < popsize.value(); i++) {
     fit.push(999);
     type[i] = new AI();
   }
   for (var i = 0; i < type.length; i++) {
     type[i].shoot();
-    var arrow = new Arrow(bow.x, bow.y, atan2(type[i].output[0] - 300, type[i].output[1] - 50), 3);
+    var arrow = new Arrow(bow.x, bow.y, atan2(type[i].output[0] - 300, type[i].output[1] - 50), 3, type[i].color);
     arrows.push(arrow);
   }
 }
@@ -42,7 +47,7 @@ function draw() {
   }
   count++;
   //----makes the new generation----\\
-  if (count > 400) {
+  if (count > 200) {
     arrows = [];
     for (var i = 0; i < fit.length / 4; i++) {
       for (var j = 0; j < fit.length; j++) {
@@ -53,7 +58,7 @@ function draw() {
       }
       nextgen = [];
       for (var i = 0; i < fit.length; i++) {
-        var n = 20 / fit[i];
+        var n = 200 / fit[i];
         for (var j = 0; j < n; j++) {
           nextgen.push(type[i])
         }
@@ -61,12 +66,21 @@ function draw() {
       nextgen[i] = type[bestnumb];
       type.splice(type[bestnumb], 1);
     }
-    target.change();
+    tcount++;
+    fit = [];
+    for (var i = 0; i < popsize.value(); i++) {
+      fit.push(999);
+    }
+    //----changes target position----\\
+    if (tcount > 5) {
+      target.change();
+      tcount = 0;
+    }
     population.selection();
     type = newgen;
     for (var i = 0; i < type.length; i++) {
       type[i].shoot();
-      var arrow = new Arrow(bow.x, bow.y, atan2(type[i].output[0] - 300, type[i].output[1] - 50), 3);
+      var arrow = new Arrow(bow.x, bow.y, atan2(type[i].output[0] - 300, type[i].output[1] - 50), 3, type[i].color);
       arrows.push(arrow);
     }
     count = 0;

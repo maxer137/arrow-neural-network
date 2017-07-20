cc542d8715ba3896f14d569b5d2e4737d84ad59a
maxer137 <maxer137@gmail.com>
2017-07-20 20:39:12 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,30 @@
 var bestnumb;
 var bow;
+var tcount = 0;   //counter for when to change targets position
 var best = 999;
-var population = 100;
-var popsize;
-var tcount = 0; //counter for when to change targets position
 var count = 0;
+var done = 0;
 var kill = 0;
+var hit = 0;
+var gen = 1;
 var ps = 20;
 var hs = 0;
 var nextgen = []; //array for nets next generation breeding
-var newgen = []; //array with children of older generation
-var arrows = []; //array for display arrows
-var type = []; //array for the nets
-var fit = []; //array for fitness values
+var newgen = [];  //array with children of older generation
+var arrows = [];  //array for display arrows
+var type = [];    //array for the nets
+var fit = [];     //array for fitness values
 
 function setup() {
   createCanvas(600, 450);
   createP('population size');
-  popsize = createSlider(10, 200, 50);
-
+  popsize = createSlider(10, 200, 50);  //population size
+  createP('mutation rate');
+  mRate = createSlider(0, 200, 10);    //mutation rate
+  createP('link to github page&nbsp;<a href="https://github.com/maxer137/arrow-neural-network/">here');
+  population = new Population();
   bow = new bow(50, 300);
   target = new Target();
-  population = new Population();
   for (var i = 0; i < popsize.value(); i++) {
     fit.push(999);
     type[i] = new AI();
@@ -41,24 +44,42 @@ function draw() {
     for (var i = arrows.length - 1; i >= 0; i--) {
       if (dist(arrows[i].x, arrows[i].y, target.x, target.y) < fit[i]) {
         fit[i] = dist(arrows[i].x, arrows[i].y, target.x, target.y);
+        if (fit[i] < 12.5) {
+          fit[i] /= 10;
+          hit++;
+        }
       }
-      arrows[i].show();
+      if (fit[i] < 12.5) {
+        arrows[i].still();
+      } else {
+        arrows[i].show();
+      }
     }
   }
   count++;
   //----makes the new generation----\\
-  if (count > 200) {
+  for (var i = 0; i < type.length; i++) {
+    if (arrows[i].x < 0 || arrows[i].x > 600 || arrows[i].y < 0 || arrows[i].y > 450 || fit[i] < 12.5) {
+      done++;
+    }
+  }
+  if (done > type.length - 1) {
+    gen++;
+    hit = 0;
     arrows = [];
     for (var i = 0; i < fit.length / 4; i++) {
-      for (var j = 0; j < fit.length; j++) {
+      for (var j = 0; j < popsize.value(); j++) {
         if (fit[j] < best) {
           best = fit[j];
+          if (fit < 12.5) {
+            fit[j] = 1;
+          }
           bestnumb = j;
         }
       }
       nextgen = [];
       for (var i = 0; i < fit.length; i++) {
-        var n = 200 / fit[i];
+        var n = 1000 / fit[i];
         for (var j = 0; j < n; j++) {
           nextgen.push(type[i])
         }
@@ -72,19 +93,26 @@ function draw() {
       fit.push(999);
     }
     //----changes target position----\\
-    if (tcount > 5) {
-      target.change();
+    if (tcount > 1) {
       tcount = 0;
+      target.change();
     }
     population.selection();
     type = newgen;
+    newgen = [];
+    arrows = [];
+    nextgen = [];
     for (var i = 0; i < type.length; i++) {
       type[i].shoot();
       var arrow = new Arrow(bow.x, bow.y, atan2(type[i].output[0] - 300, type[i].output[1] - 50), 3, type[i].color);
       arrows.push(arrow);
     }
+    done = 0;
     count = 0;
+
   }
+  done = 0;
+  text("generation:" + gen, 0, 450);
   bow.show();
   fill(255);
 }

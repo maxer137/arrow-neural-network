be944d682b931a175da3a1b7d526bb0b5d6a8806
maxer137 <maxer137@gmail.com>
2017-07-20 20:37:34 +0200

    Update arrow.js

--- arrow.js
+++ arrow.js
@@ -17,12 +17,25 @@ function Arrow(x, y, r, v, c) {
     this.yforce += 0.06;
     fill(200, 200, 200);
     imageMode(CORNER)
-    fill(this.color, 100)
+    fill(this.color)
     noStroke();
-    rect(0, 0, 30, 10)
+    rect(0, 0, 40, 5)
     pop();
     this.r = atan2(this.yforce, this.xforce);
     this.x += this.xforce * 4;
     this.y += this.yforce * 4;
+  }
+  this.still = function() {
+    push();
+    angleMode(DEGREES);
+    translate(this.x, this.y);
+    rotate(this.r);
+    //console.log(tan(this.xforce / this.yforce))
+    fill(200, 200, 200);
+    imageMode(CORNER)
+    fill(this.color, 100)
+    noStroke();
+    rect(0, 0, 40, 5)
+    pop();
   }
 }

f8b5ed9d7a0ce9f1985af6ed88988c3998dab169
maxer137 <maxer137@gmail.com>
2017-07-24 18:00:16 +0200

    Update arrow.js

--- arrow.js
+++ arrow.js
@@ -8,32 +8,22 @@ function Arrow(x, y, r, v, c) {
   this.yforce = sin(this.r) * this.v;
   this.color = c;
 
+  this.update = function() {
+    this.r = atan2(this.yforce, this.xforce);
+    this.x += this.xforce * 4;
+    this.y += this.yforce * 4;
+    this.yforce += 0.06;
+  }
+
   this.show = function() {
     push();
     angleMode(DEGREES);
     translate(this.x, this.y);
     rotate(this.r);
     //console.log(tan(this.xforce / this.yforce))
-    this.yforce += 0.06;
     fill(200, 200, 200);
     imageMode(CORNER)
     fill(this.color)
-    noStroke();
-    rect(0, 0, 40, 5)
-    pop();
-    this.r = atan2(this.yforce, this.xforce);
-    this.x += this.xforce * 4;
-    this.y += this.yforce * 4;
-  }
-  this.still = function() {
-    push();
-    angleMode(DEGREES);
-    translate(this.x, this.y);
-    rotate(this.r);
-    //console.log(tan(this.xforce / this.yforce))
-    fill(200, 200, 200);
-    imageMode(CORNER)
-    fill(this.color, 100)
     noStroke();
     rect(0, 0, 40, 5)
     pop();
